3200339ebbac6f5c9fe63c4bd5b68e55e53e152a
Samz0001 <sishere23@gmail.com>
2024-05-07 20:54:30 +0530

    FIrst Commit

---
+++ components/CustomButton.tsx
@@ -0,0 +1,21 @@
+// CustomButton.tsx
+
+import { CustomButtonProps } from '@/types';
+import React from 'react';
+
+const CustomButton: React.FC<CustomButtonProps> = ({ title, containerStyles, handleClick }) => {
+  return (
+    <button
+      disabled={false}
+      type="button"
+      className={`custom-btn ${containerStyles}`}
+      onClick={handleClick}
+    >
+      <span className="flex-1">
+        {title}
+      </span>
+    </button>
+  );
+}
+
+export default CustomButton;
