c7e7ebad0595f60e4d39a107837239e9d6950150
Samz0001 <sishere23@gmail.com>
2024-05-08 21:38:27 +0530

    ComboBox

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -3,11 +3,11 @@
 import { CustomButtonProps } from '@/types';
 import React from 'react';
 
-const CustomButton: React.FC<CustomButtonProps> = ({ title, containerStyles, handleClick }) => {
+const CustomButton: React.FC<CustomButtonProps> = ({ title, containerStyles, handleClick ,btnType}) => {
   return (
     <button
       disabled={false}
-      type="button"
+      type={btnType || "button"}
       className={`custom-btn ${containerStyles}`}
       onClick={handleClick}
     >
---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,19 @@
+"use client"
+import { SearchManufacturerProps } from '@/types'
+import React from 'react'
+import {Combobox,Transition} from "@headlessui/react";
+const SearchManufacturer = ({manufacturer,setManufacturer}:SearchManufacturerProps) => {
+  return (
+    <div className='search-manufacturer'>
+    <Combobox>
+        <div className='relative w-full'>
+            <Combobox.Button>
+
+            </Combobox.Button>
+        </div>
+    </Combobox>
+    </div>
+  )
+}
+
+export default SearchManufacturer
